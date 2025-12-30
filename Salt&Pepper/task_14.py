fd432a577c60febbddc1bd52f2a7b9d52ca55012
FroGGlik <denis.klaus2007@yandex.ru>
2025-12-30 21:26:01 +0300

    complite 14 task

--- Salt&Pepper/task_14.py
+++ Salt&Pepper/task_14.py
@@ -7,3 +7,30 @@ evens = EvenNumbers(5)
 for num in evens:
 print(num) # Должно вывести 0, 2, 4, 6, 8
 '''
+class EvenNumbers():
+    def __init__(self, n: int):
+        self.n = n
+        self.count = 0
+        self.counter = 0
+
+    def __iter__(self):
+        self.count = 0
+        self.counter = 0
+        return self
+
+    def __next__(self):
+        if self.count >= self.n:
+            raise StopIteration
+        value = 2 * self.counter 
+        self.count += 1
+        self.counter += 1
+        return value
+
+
+try:
+    evens = EvenNumbers(5)
+
+    for num in evens:
+        print(num) # Должно вывести 0, 2, 4, 6, 8
+except TypeError as ex:
+    print(f'Ваша ошибка: {ex}')
