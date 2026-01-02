00c5065ff31fdc374d4eec2ae3a59ec25e41361c
FroGGlik <denis.klaus2007@yandex.ru>
2026-01-02 19:07:26 +0300

    fix bugs

--- Salt&Pepper/task_01.py
+++ Salt&Pepper/task_01.py
@@ -1,26 +1,14 @@
 import re
 
-string = input('Введите текст: ').lower()
-pattern = r'[a-zA-Z]'
-string_only_alph = re.findall(pattern, string)
+def is_palindrome(text: str):
+    if not isinstance(text, str):
+        return False
+    pattern = r'[a-zA-Z]'
+    string_only_alph = re.findall(pattern, text.lower())
+    return string_only_alph == string_only_alph[::-1]
 
-# 1 способ через цикл while
-left, right = 0, len(string_only_alph) - 1
-flag = True
-while left < right:
-    if string_only_alph[left] != string_only_alph[right]:
-        flag = False
-        break
-    left += 1
-    right -= 1
-if flag:
-    print("Это палиндром")
-else:
-    print("Это не палиндром")
-
-# 2 способ с изпользованием среза, с добавлением шабона
-reverse_string = string_only_alph[::-1]
-if string_only_alph == reverse_string:
-    print("Это палиндром")
-else:
-    print("Это не палиндром")
+print(is_palindrome("A man, a plan, a canal -- Panama"))
+print(is_palindrome("Madam, I'm Adam!"))
+print(is_palindrome(333))
+print(is_palindrome(None))
+print(is_palindrome("Abracadabra"))
