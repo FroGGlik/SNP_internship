1f7a79123c6977d87dcd4f386e6f262571b8d54c
FroGGlik <denis.klaus2007@yandex.ru>
2026-01-16 22:08:40 +0300

    fix 2 task

--- task_02.py
+++ task_02.py
@@ -2,20 +2,28 @@ def coincidence(array=None, band=None) -> list:
     if array is None:
         array = []
     if band is None:
-        array = []
+        band = []
 
-    if not isinstance(band, (list, tuple)) or len(band) != 2:
+    if isinstance(band, range):
+        low, high = band.start, band.stop
+    elif isinstance(band, (list, tuple)) and len(band) == 2:
+        low, high = band[0], band[1]
+    else:
         return []
-    
-    low, high = band[0], band[1]
+
+    if low >= high:
+        return []
+
     result = []
 
     for element in array:
         if isinstance(element, (int, float)):
             if low <= element < high:
                 result.append(element)
+
     return result
 
-print(coincidence([1, 2, 3, 4, 5], (3, 6)))
-print(coincidence())
-print(coincidence([None, 1, 'foo', 4, 2, 2.5], (1, 4))) 
+
+print(coincidence([1, 2, 3, 4, 5], range(3, 6)))  # Теперь: [3, 4, 5]
+print(coincidence())  # []
+print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)))  # [1, 2, 2.5]
