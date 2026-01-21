96e8ddac5ddfdf302c8fb3bc00040fd72610f5cd
FroGGlik <denis.klaus2007@yandex.ru>
2026-01-22 00:49:37 +0300

    fix 12 task

--- task_12.py
+++ task_12.py
@@ -1,10 +1,9 @@
 class Dessert:
     def __init__(self, name='Unnamed', calories=0):
         self._name = name
-        self._calories = calories
-        self._is_healthy = None
-        self._is_delicious = True
-    
+        self._calories = 0
+        self.calories = calories
+
     @property
     def name(self):
         return self._name
@@ -16,12 +15,18 @@ class Dessert:
     @property
     def calories(self):
         return self._calories
-        
+
     @calories.setter
     def calories(self, value):
-        if not isinstance(value, (int, float)):
-            raise ValueError("Калорийность должна быть числом.")
-        self._calories = value
+        if isinstance(value, str):
+            if value.isdigit():
+                self._calories = int(value)
+            else:
+                self._calories = 0
+        elif isinstance(value, int):
+            self._calories = value
+        else:
+            self._calories = 0
 
     def get_info(self):
         return self._name, self._calories
@@ -36,13 +41,10 @@ class Dessert:
         self._is_healthy = self._calories < 200
     
     def __str__(self):
-        info = (
-            f'Название блюда: {self._name}\n'
-            f'Калорийность блюда: {self._calories}\n'
-            f'Здоровое ли блюдо: {"Да" if self.is_healthy() else "Нет"}\n'
-            f'Вкусное ли блюдо: {"Да" if self.is_delicious() else "Нет"}'
-        )
-        return info
+        return (f'Название блюда: {self._name}\n'
+                f'Калорийность блюда: {self._calories}\n'
+                f'Здоровое ли блюдо: {"Да" if self.is_healthy() else "Нет"}\n'
+                f'Вкусное ли блюдо: {"Да" if self.is_delicious() else "Нет"}')
 
 
 class JellyBean(Dessert):
