6a99bd15b4e0889cbadaab0e7c856f9af7165a53
FroGGlik <denis.klaus2007@yandex.ru>
2026-03-29 11:59:19 +0300

    fix 12 task

--- task12_2.py
+++ task12_2.py
@@ -1,12 +1,12 @@
 class Dessert:
-    def __init__(self, name='Unnamed', calories=None):
+    def __init__(self, name=None, calories=None):
         self._name = name
-        self.calories = calories
+        self._calories = calories
 
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, value):
         self._name = value
@@ -17,25 +17,13 @@ class Dessert:
     
     @calories.setter
     def calories(self, value):
-        print(f"Setting calories to {value}")
-        if isinstance(value, str):
-            if value.isdigit():
-                self._calories = int(value)
-            else:
-                self._calories = 0
-        elif isinstance(value, int):
-            self._calories = value
-        else:
-            self._calories = 0
+        self._calories = value
     
     def is_healthy(self):
-        return self._calories < 200
+        return self._calories is not None and self._calories < 200
     
     def is_delicious(self):
         return True
-    
-    def set_healthy(self):
-        self._is_healthy = self._calories < 200
     
     def __str__(self):
         return (f'Название блюда: {self._name}\n'
@@ -86,3 +74,17 @@ try:
     print(dessert4)
 except TypeError:
     print('Введите правильное кол-во аргументов')
+
+
+dessert5 = Dessert("Vanilla Ice Cream", 250)
+
+print("Название:", dessert5.name)
+print("Калории:", dessert5.calories)
+
+dessert5.name = "Strawberry Sorbet"
+dessert5.calories = 180
+
+print("Обновленное название:", dessert5.name)
+print("Обновленные калории:", dessert5.calories)
+
+print("Это полезно?", dessert5.is_healthy())
